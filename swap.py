975925f6b7dfd44d7e1a48cfcbd48a6766884fb8
BoIv1412 <bojan.ivanovski@studentambassadors.com>
2024-05-16 14:22:58 +0200

    new feature with num2words

--- swap.py
+++ swap.py
@@ -1,3 +1,7 @@
+from num2words import num2words
+
 def number(num1, num2): 
+    num1 = num2words(num1)
+    num2 = num2words(num2)
     numbers = print(f'a = {num1}, b = {num2}')
     return numbers
