f0e5e107e91cb0614753615d0c33fb3d9fbd1757
BoIv1412 <bojan.ivanovski@studentambassadors.com>
2024-05-23 14:59:56 +0200

    deleted

--- extras/desc.py
+++
@@ -1,5 +0,0 @@
-def descending(number):
-    i=int(number)
-    while i>0:
-        print(i)
-        i=i-1
--- main.py
+++
@@ -1,9 +0,0 @@
-import swap
-from extras import desc
-
-num1 = int(input('Enter the first number: '))
-num2 = int(input('Enter the second number: '))
-swap.number(num1,num2)
-
-n = int(input('Enter a number: '))
-desc.descending(n)
