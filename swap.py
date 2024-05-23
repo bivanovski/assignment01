e3ff83262fc6f9bfb38efba0ba1dec3f27808615
BoIv1412 <bojan.ivanovski@studentambassadors.com>
2024-05-23 15:07:28 +0200

    feature

---
+++ swap.py
@@ -0,0 +1,9 @@
+import num2words
+
+def swap(num1,num2):
+    if (num1 > 0 and num1< 4) and (num2 > 0 and num2 < 4):
+        print(f'a = {num2words(num1)}, b = {num2words(num2)}')
+    else:
+        print('Out of range')    
+        
+    
